44e98362f88b60a356b669b6f29738d39dd87112
litonofficial61 <litonofficial61gmail.com>
2023-08-13 11:35:14 +0600

    simple problems

---
+++ problems.js
@@ -0,0 +1,141 @@
+// problem -1
+// var momGivenMoney = 1000;
+// var orangePrice = 400;
+// var applePrice = 300;
+// var totalCost = orangePrice + applePrice;
+// var returnMoney = momGivenMoney - totalCost;
+// console.log("Shopkeeper will return : ",returnMoney, "Taka");
+
+// problem -2
+// var mathematics = 75.25;
+// var biology = 65;
+// var chemistry = 80;
+// var physics = 35.45;
+// var bengali = 99.50;
+// var totalMarks = mathematics + biology + chemistry + physics + bengali;
+// var averageMarks = totalMarks / 5;
+// averageMarks = averageMarks.toFixed(2)
+// console.log(averageMarks);
+
+// problem -3
+// var firstLine = "I am going to be";
+// var secondLine = "an awesome web developer";
+// var fullLine = firstLine + " " + secondLine;
+// console.log(fullLine);
+
+// problem -4
+// var number = 119;
+// var remainder = 119 % 5;
+// console.log(remainder);
+
+// problem -5
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var index = fruits.indexOf('Banana');
+// fruits[index] = 'Mango';
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+// problem -6
+// var number1 = 13;
+// var number2 = 79;
+// var number3 = 45;
+// if(number1 > number2 && number1 > number3 ){
+//     console.log('The largest number is number1');
+// }
+// else if(number2 > number1 && number2 > number3){
+//     console.log('The largest number is number2');
+// }
+// else('The largest number is number3');
+
+// problem -7
+// var side1 = 9;
+// var side2 = 8;
+// var side3 = 9;
+// if(side1 == side2 || side1 == side3 || side2 == side3){
+//     console.log('The triangle is Isosceles');
+// }
+// else('The triangle is not Isosceles');
+
+// problem -8
+// var yourScore = 85;
+// var tomScore = 66;
+// var janeScore = 95;
+// var peterScore = 56;
+// var johnScore = 40;
+
+// if(yourScore >= 80){
+//     console.log("Your grade is : ", 'A');
+// }
+// else if(yourScore >= 60){
+//     console.log("Your grade is : ", 'B');
+// }
+// else if(yourScore >= 50){
+//     console.log("Your grade is : ", 'C');
+// }
+// else if(yourScore >= 40){
+//     console.log("Your grade is : ", 'D');
+// }
+// else("Your grade is : ", 'F');
+
+
+// if(tomScore >= 80){
+//     console.log("Tom's grade is : ", 'A');
+// }
+// else if(tomScore>= 60){
+//     console.log("Tom's grade is : ", 'B');
+// }
+// else if(tomScore >= 50){
+//     console.log("Tom's grade is : ", 'C');
+// }
+// else if(tomScore >= 40){
+//     console.log("Tom's grade is : ", 'D');
+// }
+// else("Tom's grade is : ", 'F');
+
+
+// if(janeScore >= 80){
+//     console.log("Jane's grade is : ", 'A');
+// }
+// else if(janeScore >= 60){
+//     console.log("Jane's grade is : ", 'B');
+// }
+// else if(janeScore >= 50){
+//     console.log("Jane's grade is : ", 'C');
+// }
+// else if(janeScore >= 40){
+//     console.log("Jane's grade is : ", 'D');
+// }
+// else("Jane's grade is : ", 'F');
+
+
+// if(peterScore >= 80){
+//     console.log("Peter's grade is : ", 'A');
+// }
+// else if(peterScore >= 60){
+//     console.log("Peter's grade is : ", 'B');
+// }
+// else if(peterScore >= 50){
+//     console.log("Peter's grade is : ", 'C');
+// }
+// else if(peterScore >= 40){
+//     console.log("Peter's grade is : ", 'D');
+// }
+// else("Peter's grade is : ", 'F');
+
+
+// if(johnScore >= 80){
+//     console.log("John's grade is : ", 'A');
+// }
+// else if(johnScore >= 60){
+//     console.log("John's grade is : ", 'B');
+// }
+// else if(johnScore >= 50){
+//     console.log("John's grade is : ", 'C');
+// }
+// else if(johnScore >= 40){
+//     console.log("John's grade is : ", 'D');
+// }
+// else("John's grade is : ", 'F');
